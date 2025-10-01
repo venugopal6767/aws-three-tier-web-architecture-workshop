0c20df43c851d0ae0600b23f8d62d3b8ea768b21
venugopal6767 <141677482+venugopal6767@users.noreply.github.com>
2025-10-01 10:32:40 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST     : process.env.DB_HOST,       // RDS endpoint
+    DB_USER     : process.env.DB_USER,       // username
+    DB_PWD      : process.env.DB_PASSWORD,   // password
+    DB_DATABASE : process.env.DB_NAME        // database name
 });
